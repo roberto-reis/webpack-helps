40aa3a2def39000904ef5d00c1c6ab125ee64ed2
José Roberto <tekinforroberto@gmail.com>
2021-02-09 22:16:16 -0300

    Webpack múltiplas entradas e saídas

---
+++ webpack-sass/src/pedido.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import './css/pedido.scss';
+import Botao from './componentes/botao/index.js';
+
+
+$(function() {
+
+    alert("Funcionado... Pedido!!!");
+
+})
--- webpack-sass/webpack.config.js
+++ webpack-sass/webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        home: './src/home.js',
+        pedido: './src/pedido.js'
+    },
     output: {
-        filename: 'script.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist/assets/js')
     },
     mode: 'production',
